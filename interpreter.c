16735832dc907a8db2ab4f0fa230f2e60a7f837a
juan <juan@cuauhtli.tlalticpac>
2014-04-14 15:33:11 -0500

    Agregamos la primer fase de parseo, si la expresion es correcta se imprime el arbol de sistaxis abstracta de la expresion

--- interpreter.c
+++ interpreter.c
@@ -40,18 +40,16 @@ int main(int argc, char *agrv[])
   mpc_parser_t *Numero   = mpc_new("numero");
   mpc_parser_t *Operador = mpc_new("operador");
   mpc_parser_t *Expr     = mpc_new("expr");
-  mpc_parser_t *miniLisp = mpc_new("miiLisp");
+  mpc_parser_t *miniLisp = mpc_new("miniLisp");
 
   /* Definimos la gramatica */
   mpca_lang(MPC_LANG_DEFAULT,
-	    "                                                     \
-              numero   : /-?[0-9]+/ ;				  \
-              operador : '+' | '-' | '*' | '/' ;                  \
-              expr     : <numero> | '(' <operador> <expr>+ ')' ;  \
-              miniLisp : /^/ <operador> <expr>+ /$/ ;             \
-            ",
-	    Numero, Operador, Expr, miniLisp);
-
+	    "numero   : /-?[0-9]+/ ;                    \
+    operador : '+' | '-' | '*' | '/' ;                  \
+    expr     : <numero> | '(' <operador> <expr>+ ')' ;  \
+    miniLisp    : /^/ <operador> <expr>+ /$/ ;             \
+  ",
+  Numero, Operador, Expr, miniLisp);
   
   /* Imprime la versión e información de salida */
   puts("miniLisp Version 0.0.0.0.1");
@@ -67,6 +65,19 @@ int main(int argc, char *agrv[])
     /* Agregamos la entrada al historial */
     add_history(input);
 
+    /* Intentamos parsear la entrada del usuario */
+    mpc_result_t r;
+    if(mpc_parse("<stdin>", input, miniLisp, &r)){
+      /* Si la entrada es correcta imprimimos el arbol */
+      /* 	de sintaxis abstracta */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Si no es correcta imprimimos el error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    
     /* Liberamos el espacio utilizado como entrada */
     free(input);
     
